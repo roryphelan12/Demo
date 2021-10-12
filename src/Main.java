16249f1a9e2e49f11da2b5c49272a32dea43eb4c
rory <rphelan@softwareinstitute.com>
2021-10-12 15:22:59 +0100

    update

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+
         System.out.println("What is the width of the wall in M?");
         double width = scan.nextDouble();
         System.out.println("What is the height of the wall in M?");
@@ -22,15 +23,29 @@ public class Main {
         double volume = Integer.parseInt(scan.next());
         System.out.println(volume);
 
+
         //assume 1 litre of paint covers 6M^2
 
         double one_litre_cost = cost / volume;
 
-        double new_area = area / 6;
-        double final_cost = (area / 6) * (one_litre_cost);
-        System.out.println(final_cost);
-        String str = "Cost to paint the wall = £" + final_cost;
+        double pre_final_cost = (area / 6) * (one_litre_cost);
+        System.out.println(pre_final_cost);
+        String str = "Cost to paint the wall = £" + pre_final_cost;
         System.out.println(str);
+
+        System.out.println("Do you want fancy paint");
+        if (scan.next() == "Yes") {
+            pre_final_cost = pre_final_cost*1.5;
+            String new_str = "Cost to paint the wall = £" + pre_final_cost;
+            System.out.println(new_str);
+        }
+        else {System.out.println(str);
+
+        }
+
+
+
+
 
     }
 
