48dd90f0912548940a42a47fcca6e2e9b8a01fe6
rory <rphelan@softwareinstitute.com>
2021-10-12 16:03:49 +0100

    update switch

--- src/Main.java
+++ src/Main.java
@@ -19,14 +19,35 @@ public class Main {
         double cost = scan.nextDouble();
         System.out.println(cost);
 
-        System.out.println("What is the volume of the paint tin?");
-        double volume = Integer.parseInt(scan.next());
+        System.out.println("What is the volume of the paint tin --- 1,2,3,4 or 5 litres");
+        int volume =0 ;
+
+        int initial_volume =  scan.nextInt();
+        switch (initial_volume) {
+            case 1:
+                 volume = 1;
+                break;
+            case 2:
+                 volume = 2;
+                break;
+            case 3:
+                 volume = 3;
+                break;
+            case 4:
+                 volume = 4;
+                break;
+            case 5:
+                 volume = 5;
+                break;
+        }
+
         System.out.println(volume);
 
 
         //assume 1 litre of paint covers 6M^2
 
         double one_litre_cost = cost / volume;
+        System.out.println(one_litre_cost);
 
         double pre_final_cost = (area / 6) * (one_litre_cost);
         System.out.println(pre_final_cost);
@@ -34,9 +55,9 @@ public class Main {
         System.out.println(str);
 
         System.out.println("Do you want fancy paint");
-        if (scan.next() == "Yes") {
-            pre_final_cost = pre_final_cost*1.5;
-            String new_str = "Cost to paint the wall = £" + pre_final_cost;
+        if (scan.next() == "yes") {
+            int final_cost = (int) (pre_final_cost*1.5);
+            String new_str = "Cost to paint the wall = £" + final_cost;
             System.out.println(new_str);
         }
         else {System.out.println(str);
