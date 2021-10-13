32cefda45ee64f40e6d132380e9469aecacf1c9e
rory <rphelan@softwareinstitute.com>
2021-10-13 13:52:17 +0100

    loops

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,52 @@
 package com.example;
 
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+
+
         Scanner scan = new Scanner(System.in);
+
+        System.out.println("Here are the colours available");
+
+
+
+        String [] paint_colours = new String[]{"red", "blue", "green","yellow"};
+        String X = "mixture";
+
+        for(String n: paint_colours) {
+            System.out.println(n);
+        }
+////// just practising loops
+        for(String n:paint_colours){
+           n  = X;
+            System.out.println(n);
+        }
+
+        System.out.println("Here are listed paint types:");
+        List<String> types = new ArrayList<>();
+        types.add("Gloss");
+        types.add("Varnish");
+        types.add("Undercoat");
+
+        for(String name:types){
+            System.out.println(name);
+        }
+
+        System.out.println("Type to add an extra");
+        types.add(scan.next());
+
+
+       // System.out.println(types.get(3))
+
+        for(String name:types){
+            System.out.println(name);
+        }
 
         System.out.println("How many walls are there?");
         int wall_number = scan.nextInt();
@@ -73,6 +114,14 @@ public class Main {
 
         }
 
+
+
+
+
+
+
+
     }
+
 
 }
